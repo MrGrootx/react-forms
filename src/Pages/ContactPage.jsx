2d0defe81cc0200c43a300933420be2894049f5d
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-13 07:36:38 +0530

    set: default value

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -36,6 +36,9 @@ const ContactPage = () => {
     handleSubmit,
     formState: { errors },
   } = useForm({
+    defaultValues: {
+      gender: "Male"
+    },
     resolver: zodResolver(validationData),
   });
 
